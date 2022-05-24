9ea920c85495a1d34a083734526f75f3dafa97ee
Victor Colon <vicbolo78@gmail.com>
2022-05-24 18:39:09 +0000

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for about view
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
 
