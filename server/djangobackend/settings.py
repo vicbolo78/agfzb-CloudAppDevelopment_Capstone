b98b70c36961617a9b6ab66dbb840b284998f571
Victor Colon <vicbolo78@gmail.com>
2023-01-25 18:00:58 -0500

    changes made from theia lab environment

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['localhost', 'localhost:8000', 'vicbolo78.us-south.cf.appdomain.cloud', 'https://vicbolo78-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 'localhost:8000', 'viclo.apic.us-east.mybluemix.net', 'https://vicbolo78-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 
 # Application definition
